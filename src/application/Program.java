7db1446e34c2c04b1c9dc6c7454daa99d00f49aa
Nicolask139 <nicolaskiy@gmail.com>
2024-07-21 12:16:34 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,17 +12,25 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== Teste 1: seller findById ===");	
-		Seller seller = sellerDao.findById(3);	
+
+		System.out.println("=== Teste 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n === Teste 2: seller findByDepartment ===");
-		
-		Department department = new Department (2, null);
+
+		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
-}}
+		System.out.println("\n === Teste 3: seller findAll ===");
+
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+	}
+}
